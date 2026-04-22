d30874ed2ae66a7a82fea931ee1dac40aaad3834
dragonfly13110 <dragonfly.forester@outlook.com>
2026-04-22 22:39:40 +0700

    main

--- scripts/extract.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const d = JSON.parse(fs.readFileSync('t:/web/npt_dashboard/tmp_districts.geojson'));
-d.features = d.features.filter(f => f.properties.pro_th === 'นครปฐม');
-fs.writeFileSync('t:/web/npt_dashboard/public/nakhon_pathom.geojson', JSON.stringify(d));
-console.log('Done:', d.features.length);
