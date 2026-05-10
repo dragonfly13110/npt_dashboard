d982b8a17569c0347a5fe1eda21b6efdbd5845b3
dragonfly13110 <dragonfly.forester@outlook.com>
2026-05-10 22:58:28 +0700

    ignore generated output cache

--- output/vite-cache/deps/react-dom.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-LKQNAKB3.js";
-import "./chunk-ROFNDEGV.js";
-import "./chunk-G3PMV62Z.js";
-export default require_react_dom();
--- output/vite-cache/deps/react-dom_client.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_client
-} from "./chunk-4PHSVEO4.js";
-import "./chunk-LKQNAKB3.js";
-import "./chunk-ROFNDEGV.js";
-import "./chunk-G3PMV62Z.js";
-export default require_client();
